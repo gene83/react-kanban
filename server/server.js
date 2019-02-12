90daaf250e32c35b7247102650221b431c09ac1b
Gene83 <gene83@protonmail.com>
2019-02-12 12:03:46 -1000

    add user auth

--- server/server.js
+++ server/server.js
@@ -7,13 +7,74 @@ const bcrypt = require('bcryptjs');
 const redis = require('connect-redis')(session);
 const flash = require('connect-flash');
 
+const User = require('../database/models/User');
+
 const PORT = process.env.PORT || 8080;
 const ENV = process.env.NODE_ENV || 'development';
+const SESSION_SECRET = process.env.SESSION_SECRET || 'kabob';
 
 const app = express();
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+app.use(
+  session({
+    store: new redis({ url: 'redis://local-host:6379', logErrors: true }),
+    secret: SESSION_SECRET,
+    resave: false,
+    cookie: { secure: ENV === 'production' }
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  return done(null, {
+    id: user.id,
+    username: user.username
+  });
+});
+
+passport.deserializeUser((user, done) => {
+  User.where('id', user.id)
+    .fetch()
+    .then(dbUser => {
+      dbUser = dbUser.toJSON();
+      return done(null, {
+        id: dbUser.id,
+        username: dbUser.username
+      }).catch(err => {
+        console.log(err);
+      });
+    });
+});
+
+passport.use(
+  new localStrategy((username, password, done) => {
+    return User.where('username', username)
+      .fetch()
+      .then(user => {
+        if (user === null) {
+          return done(null, false, {
+            message: `user: ${username} does not exist`
+          });
+        } else {
+          user = user.toJSON();
+          bcrypt.compare(password, user.password).then(res => {
+            if (res) {
+              return done(null, user);
+            } else {
+              return done(null, false, { message: 'incorrect password' });
+            }
+          });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  })
+);
 
 app.use(express.static('public'));
 
